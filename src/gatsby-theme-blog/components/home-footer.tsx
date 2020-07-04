664f6cdf2746cace00b167118562ef5b72f656cf
Alexandr Maximov <maks190@gmail.com>
2020-07-04 13:03:55 +0200

    Simplify footer

---
+++ src/gatsby-theme-blog/components/home-footer.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { css } from "theme-ui"
+
+const Footer = () => (
+  <footer
+    css={css({
+      mt: 4,
+      pt: 3,
+    })}
+  >
+  </footer>
+)
+
+export default Footer
